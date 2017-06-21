80cdf02f21dafa15634335e0f25a81c2d3cba34a
Danny Baumann <dannybaumann@web.de>
2017-06-21 14:58:21 -0500

    wayne-common: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- wayne.mk
+++ wayne.mk
@@ -32,7 +32,8 @@ $(call inherit-product, $(LOCAL_PATH)/properties.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-arrow
 
 # Audio
 PRODUCT_COPY_FILES += \
