b7dedeb1c093147d91dc80dffb7b555eb26619ea
TH779 <i@779.moe>
2021-06-21 20:36:53 +0800

    wayne-common: Arrow-ify
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I79753ccbfa67eaa0a874608ca190cdecfd0a9a72

--- wayne.mk
+++ wayne.mk
@@ -32,8 +32,7 @@ $(call inherit-product, $(LOCAL_PATH)/properties.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -43,8 +42,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2160
-TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Consumerir
 PRODUCT_PACKAGES += \
