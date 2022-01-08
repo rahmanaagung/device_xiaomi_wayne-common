36c4839ccec6ab906855158a397d2f80b6378bb4
NurKeinNeid <mralexman3000@gmail.com>
2022-01-08 18:21:33 +0100

    wayne-common: Derp it
    Change-Id: I3284b8644aba739a0eda64be0a32f371f92ba8bb
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- wayne.mk
+++ wayne.mk
@@ -13,7 +13,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-derp
 
 # Audio
 PRODUCT_COPY_FILES += \
